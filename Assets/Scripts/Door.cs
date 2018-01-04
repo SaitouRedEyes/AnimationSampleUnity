e2739be9ce8f45cf0e421d983997a58c3ef9ba42
SaitouRedEyes <zeca.saitou@gmail.com>
2018-01-04 12:08:56 -0200

    Animation Sample Unity
    Animation Sample with Unity.

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            animator.SetBool("Open", true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            animator.SetBool("Open", false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();	
+	}
+
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.localPosition += Vector3.forward * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.localPosition += Vector3.back * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.localPosition += Vector3.left * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.localPosition += Vector3.right * Time.deltaTime;
+        }
+    }
+}
